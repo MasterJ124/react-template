e76c47a8fb6ff0ffbb42503a00ec5d94be3e4154
jiyunjie <jiyunjie@netjoy.com>
2023-02-20 17:41:44 +0800

    redux开启持久化

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,30 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storageSession from 'redux-persist/lib/storage/session'; // defaults to sessionStorage for web
+
 import userReducer from '@/features/userInfoSlice';
 
-export const store = configureStore({
-  reducer: {
+// 持久化配置
+const persistConfig = {
+  key: 'MCN_MANAGE',
+  storage: storageSession,
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers({
+    // 合并切片
     userInfo: userReducer,
-  },
+  }),
+);
+
+// 创建store
+export const store = configureStore({
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
 
 // 从 store 本身推断出 `RootState` 和 `AppDispatch` 类型
 export type RootState = ReturnType<typeof store.getState>;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,14 @@
 import type { FC } from 'react';
-import { useAppSelector } from '@/app/hooks';
+import { useAppSelector, useAppDispatch } from '@/app/hooks';
+import { setUserToken } from '@/features/userInfoSlice';
+import { Button } from 'antd';
 
 import logo from '@/logo.svg';
 import '@/App.css';
 
 const Home: FC = () => {
-  const userInfo = useAppSelector((state) => state.userInfo.info);
+  const dispatch = useAppDispatch();
+  const userInfo = useAppSelector((state) => state.userInfo);
 
   return (
     <div className="App">
@@ -15,7 +18,11 @@ const Home: FC = () => {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <p>{process.env.REACT_APP_API_URL}</p>
+        <Button onClick={() => dispatch(setUserToken(''))}>清空</Button>
         <p>{userInfo.token}</p>
+        <Button onClick={() => dispatch(setUserToken('c616436614bacb7afaa6ea65cb8756e7'))}>
+          添加token
+        </Button>
         <a
           className="App-link"
           href="https://reactjs.org"
