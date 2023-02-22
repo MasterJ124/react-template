901face74537774d6267c7be4100a0b0b09da524
liyihu <liyihu@netjoy.com>
2023-02-22 16:05:13 +0800

    查询

--- src/pages/merchant/enterprise/index.tsx
+++ src/pages/merchant/enterprise/index.tsx
@@ -1,9 +1,10 @@
 import type { FC } from 'react';
-import { Form, Input, Button, Row, Col, message } from 'antd';
+import { Form, Input, Button, Row, Col, message, Select } from 'antd';
 import { useState } from 'react';
 import '@/pages/login/index.less';
 import UserTable from './table';
 import { getMerchantList } from '@/api/merchant';
+import { MERCHANT_TYPE, AUDIT_STATUS, MERCHANT_STATUS } from '@/utils/config';
 
 const Home: FC = () => {
   const [form] = Form.useForm();
@@ -50,17 +51,17 @@ const Home: FC = () => {
             </Col>
             <Col span={6}>
               <Form.Item name="type" label="商户类型">
-                <Input placeholder="请输入" />
+                <Select placeholder="请选择" options={MERCHANT_TYPE} />
               </Form.Item>
             </Col>
             <Col span={6}>
               <Form.Item name="audit_status" label="审核状态">
-                <Input placeholder="请输入" />
+                <Select placeholder="请选择" options={AUDIT_STATUS} />
               </Form.Item>
             </Col>
             <Col span={6}>
               <Form.Item name="merchant_status" label="商户状态">
-                <Input placeholder="请输入" />
+                <Select placeholder="请选择" options={MERCHANT_STATUS} />
               </Form.Item>
             </Col>
             <Col
