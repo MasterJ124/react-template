35951c9c4f30d0c96c2a07fb2a1f77e8a49f4935
liyihu <liyihu@netjoy.com>
2023-02-22 15:21:04 +0800

    优化

--- src/pages/merchant/enterprise/table.tsx
+++ src/pages/merchant/enterprise/table.tsx
@@ -5,25 +5,27 @@ const { Column, ColumnGroup } = Table;
 interface PropsType {
   list: any;
 }
-const UserTable: React.FC<PropsType> = ({ list }) => (
-  <Table dataSource={list}>
-    <Column title="ID" dataIndex="id" key="id" />
-    <Column title="公司名称" dataIndex="user" key="user" />
-    <Column title="统一社会信用代码" dataIndex="userId" key="userId" />
-    <Column title="营业期限" dataIndex="mobile" key="mobile" />
-    <Column title="审核状态" dataIndex="name" key="name" />
-    <Column title="商户状态" dataIndex="shop" key="shop" />
-    <Column title="经营" dataIndex="status" key="status" />
-    <Column
-      title="操作"
-      render={(_: any, record: any) => (
-        <Space size="middle">
-          <a>禁用</a>
-          <a>启用</a>
-        </Space>
-      )}
-    />
-  </Table>
-);
+const UserTable: React.FC<PropsType> = ({ list }) => {
+  return (
+    <Table dataSource={list}>
+      <Column title="ID" dataIndex="id" key="id" />
+      <Column title="公司名称" dataIndex="user" key="user" />
+      <Column title="统一社会信用代码" dataIndex="userId" key="userId" />
+      <Column title="营业期限" dataIndex="mobile" key="mobile" />
+      <Column title="审核状态" dataIndex="name" key="name" />
+      <Column title="商户状态" dataIndex="shop" key="shop" />
+      <Column title="经营" dataIndex="status" key="status" />
+      <Column
+        title="操作"
+        render={(_: any, record: any) => (
+          <Space size="middle">
+            <a>禁用</a>
+            <a>启用</a>
+          </Space>
+        )}
+      />
+    </Table>
+  );
+};
 
 export default UserTable;
